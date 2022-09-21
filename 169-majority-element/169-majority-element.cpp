95b32fe1013ad13219f027bcdba28242d23cf87a
Prince Chaurasia <prncchaurasia@gmail.com>
2022-09-21 21:29:48 +0530

    Time: 49 ms (13.51%), Space: 19.5 MB (92.15%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> um;
-        for(auto i:nums)
-        {
-            um[i]++;
-            if(um[i]>nums.size()/2) return i;
-        }
-        return -1;
-    }
+    //1st Approach
+     int majorityElement(vector<int>& nums) {
+    //     unordered_map<int,int> um;
+    //     for(auto i:nums)
+    //     {
+    //         um[i]++;
+    //         if(um[i]>nums.size()/2) return i;
+    //     }
+    //     return -1;
+    // }
+    //2nd Approach
+    sort(nums.begin(),nums.end());
+    return nums[nums.size()/2];
+     }
 };
