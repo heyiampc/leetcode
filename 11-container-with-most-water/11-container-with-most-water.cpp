6cbe247e6e916180e8e5971387b22bfb502d7249
Prince Chaurasia <prncchaurasia@gmail.com>
2022-09-24 16:59:50 +0530

    Time: 210 ms (19.64%), Space: 59 MB (80.10%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -5,13 +5,11 @@ public:
         if(n==1) return n;
         int x=0;
         int y=n-1;
-        if(n==2) return min(height[x],height[y]);
         int area=0;
         while(x<y)
         {
             int l=min(height[x],height[y]);
-            int b=y-x;
-            area=max(l*b,area);
+            area=max(l*(y-x),area);
             if(l==height[x]) x++; else y--;
         }
         return area;
