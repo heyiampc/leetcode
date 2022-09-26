f6c89c99b684649ecbc6d3d969ba57940bed3114
Prince Chaurasia <prncchaurasia@gmail.com>
2022-09-26 19:59:57 +0530

    Time: 109 ms (16.44%), Space: 28.2 MB (67.38%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -7,9 +7,9 @@ public:
             v[x%60]+=1;
         }
         long long count=0;
-        count+=((v[0]-1)*v[0])/2;
+        count+=((v[0]-1)*v[0])/2; // For calculating 0 and 30 remainder
         count+=((v[30]-1)*v[30])/2;
-        for(int i=1;i<=29;i++)
+        for(int i=1;i<=29;i++) // running till n/2 to avoid mirror duplicates
         {
             count+=v[i]*v[60-i];
         }
