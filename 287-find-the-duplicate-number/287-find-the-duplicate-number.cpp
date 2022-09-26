03cccc70d205700bc74b2e24b5372a53937ec5e1
Prince Chaurasia <prncchaurasia@gmail.com>
2022-09-26 20:43:20 +0530

    Time: 332 ms (19.75%), Space: 61.3 MB (29.77%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        //Floyd  Cycle Detection
+        int n=nums.size();
+        if(n<=1) return 0;
+        int slow=nums[0];
+        int fast=nums[0];
+        do
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        slow=nums[0];
+        while(fast!=slow)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+};
