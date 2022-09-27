a106a367c840edd3e6f074bac77f9594ed77e2b2
Prince Chaurasia <prncchaurasia@gmail.com>
2022-09-27 23:02:05 +0530

    Time: 40 ms (51.54%), Space: 16.4 MB (96.57%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -3,13 +3,13 @@ public:
     int jump(vector<int>& nums) {
         int jump=0;
         int pos=0;
-        int des=0;
+        int max_reach=0;
         for(int i=0;i<nums.size()-1;i++)
         {
-            des=max(des,i+nums[i]);
+            max_reach=max(max_reach,i+nums[i]);
             if(pos==i)
             {
-                pos=des;
+                pos=max_reach;
                 jump++;
             }
         }
