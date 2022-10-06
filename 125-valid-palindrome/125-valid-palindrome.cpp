88e23b58eb4bc4c7ee23e01c27a2e861d2e0aee9
Prince Chaurasia <prncchaurasia@gmail.com>
2022-10-06 20:48:39 +0530

    Time: 11 ms (49.06%), Space: 7.3 MB (77.72%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -7,22 +7,33 @@ public:
         return false;
     }
     bool isPalindrome(string s) {
-         vector<char> v{};
-        for(int i=0;i<s.length();i++)
+        //Using Extra Space
+    //      vector<char> v{};
+    //     for(int i=0;i<s.length();i++)
+    //     {
+    //         if(isValid(s[i])) v.pb(s[i]); 
+    //     }
+    //     if(v.empty()) return true;
+    //     return helper(v);
+    //     int j=v.size()-1;
+    // for(int i=0;i<v.size();i++)
+    // {
+    //       v[i]= tolower(v[i]);
+    //       v[j]= tolower(v[j]);
+    //     if(v[i]!=v[j--])
+    //     {
+    //         return false;
+    //     }
+    // }
+    //     return true;
+
+        //Without Extra Space
+        for(int i=0,j=s.length()-1;i<j;i++,j--)
         {
-            if(isValid(s[i])) v.pb(s[i]); 
+            while(!isalnum(s[i]) && i<j) i++;
+            while(!isalnum(s[j]) && i<j) j--;
+            if(tolower(s[i])!=tolower(s[j])) return false;
         }
-        if(v.empty()) return true;
-        int j=v.size()-1;
-    for(int i=0;i<v.size();i++)
-    {
-          v[i]= tolower(v[i]);
-          v[j]= tolower(v[j]);
-        if(v[i]!=v[j--])
-        {
-            return false;
-        }
-    }
         return true;
     }
 };
