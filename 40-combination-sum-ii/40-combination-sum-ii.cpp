ad9625aaf84ffebbd940b40cb20b0051edac0d14
Prince Chaurasia <prncchaurasia@gmail.com>
2022-10-07 11:54:57 +0530

    Time: 6 ms (81.06%), Space: 13.6 MB (19.24%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+     void helperfun(int idx,vector<int> arr, vector<int> &ds,vector<vector<int>> &ans,int target)
+    {
+            if(target==0) 
+            {
+                ans.push_back(ds);
+                return;
+            }
+         for(int i=idx;i<arr.size();i++)
+         {
+            if(i>idx && arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) break;
+            ds.push_back(arr[i]);
+            helperfun(i+1,arr,ds,ans,target-arr[i]);
+            ds.pop_back();
+         }
+        // helperfun(i+1,arr,ds,ans,target);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int> ds;
+        vector<vector<int>> ans;
+        helperfun(0,candidates,ds,ans,target);
+        return ans;
+    }
+};
