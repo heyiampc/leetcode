10b07c86e11fc7654ed5f66cbf51888923ee0602
Prince Chaurasia <prncchaurasia@gmail.com>
2022-10-08 22:04:17 +0530

    Time: 4 ms (68.10%), Space: 8.2 MB (19.28%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,33 @@
+#define pb push_back
+#define pbb pop_back
+class Solution {
+public:
+    //Approach 1
+    void helperfun(vector<vector<int>> &ans,vector<int> &ds,vector<int> arr, int freq[])
+    {
+        if(ds.size()==arr.size())
+        {
+            ans.pb(ds);
+            return;
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            if(!freq[i])
+            {
+            ds.pb(arr[i]);
+            freq[i]=1;
+            helperfun(ans,ds,arr,freq);
+            freq[i]=0;
+            ds.pbb();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        int freq[nums.size()];
+        for(int i=0;i<nums.size();i++) freq[i]=0;
+        helperfun(ans,ds,nums,freq);
+        return ans;
+    }
+};
