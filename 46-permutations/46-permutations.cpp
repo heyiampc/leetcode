b629ed04101ee4100c28408b87dae2c6dfce86df
Prince Chaurasia <prncchaurasia@gmail.com>
2022-10-08 22:37:35 +0530

    Time: 12 ms (14.02%), Space: 7.6 MB (91.72%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,30 +3,49 @@
 class Solution {
 public:
     //Approach 1
-    void helperfun(vector<vector<int>> &ans,vector<int> &ds,vector<int> arr, int freq[])
+    // void helperfun(vector<vector<int>> &ans,vector<int> &ds,vector<int> arr, int freq[])
+    // {
+    //     if(ds.size()==arr.size())
+    //     {
+    //         ans.pb(ds);
+    //     }
+    //     for(int i=0;i<arr.size();i++)
+    //     {
+    //         if(!freq[i])
+    //         {
+    //         ds.pb(arr[i]);
+    //         freq[i]=1;
+    //         helperfun(ans,ds,arr,freq);
+    //         freq[i]=0;
+    //         ds.pbb();
+    //         }
+    //     }
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     vector<vector<int>> ans;
+    //     vector<int> ds;
+    //     int freq[nums.size()];
+    //     for(int i=0;i<nums.size();i++) freq[i]=0;
+    //     helperfun(ans,ds,nums,freq);
+    //     return ans;
+    // }
+    void helperfun(int index, vector<vector<int>> &ans, vector<int> &arr)
     {
-        if(ds.size()==arr.size())
+        if(index==arr.size())
         {
-            ans.pb(ds);
+            ans.pb(arr);
         }
-        for(int i=0;i<arr.size();i++)
+        for(int i=index;i<arr.size();i++)
         {
-            if(!freq[i])
-            {
-            ds.pb(arr[i]);
-            freq[i]=1;
-            helperfun(ans,ds,arr,freq);
-            freq[i]=0;
-            ds.pbb();
-            }
+            swap(arr[index],arr[i]);
+            helperfun(index+1,ans,arr);
+            swap(arr[index],arr[i]);
         }
     }
-    vector<vector<int>> permute(vector<int>& nums) {
+    //Approach 2 Using Swap in recursion
+     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++) freq[i]=0;
-        helperfun(ans,ds,nums,freq);
+        helperfun(0,ans,nums);
         return ans;
     }
 };
