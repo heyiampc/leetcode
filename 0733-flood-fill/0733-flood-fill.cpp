eb72918e49746c497513576779b0b9e85f682b5c
Prince Chaurasia <prncchaurasia@gmail.com>
2022-10-10 21:37:11 +0530

    Time: 15 ms (65.04%), Space: 13.7 MB (98.87%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void fill(vector<vector<int>>& image, int x, int y, int color, int oldColor,int n, int m)
+    {
+        if(x<0 || y<0 || x==n || y==m || image[x][y]!=oldColor) return;
+        image[x][y]=color;
+        fill(image,x,y+1,color,oldColor,n,m);
+        fill(image,x+1,y,color,oldColor,n,m);
+        fill(image,x,y-1,color,oldColor,n,m);
+        fill(image,x-1,y,color,oldColor,n,m);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int oldColor=image[sr][sc];
+        if(color==oldColor) return image;
+        fill(image,sr,sc,color,oldColor,image.size(),image[0].size());
+        return image;
+    }
+};
