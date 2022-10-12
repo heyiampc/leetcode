bf1395e906a9a085a898e32022af350e940478b5
Prince Chaurasia <prncchaurasia@gmail.com>
2022-10-12 21:24:14 +0530

    Time: 5 ms (20.15%), Space: 5.8 MB (91.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    int fun(int m, int n, int dp[100][100])
-    {
-        if(m==0 &&n==0) return 1;
-        if(m<0 || n<0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up=fun(m-1,n,dp);
-        int left=fun(m,n-1,dp);
-        return dp[m][n]=up+left;
-    }
+    // int fun(int m, int n, int dp[100][100])
+    // {
+    //     if(m==0 &&n==0) return 1;
+    //     if(m<0 || n<0) return 0;
+    //     if(dp[m][n]!=-1) return dp[m][n];
+    //     int up=fun(m-1,n,dp);
+    //     int left=fun(m,n-1,dp);
+    //     return dp[m][n]=up+left;
+    // }
     int uniquePaths(int m, int n) {
-        int dp[100][100];
-        memset(dp,-1,sizeof dp);
-        return fun(m-1,n-1,dp);
+        int dp[m][n];
+        // memset(dp,-1,sizeof dp);
+        // return fun(m-1,n-1,dp);
+        //Tabulation
+        dp[0][0]=1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0) dp[0][0]=1;
+                else{
+                    int up=0;
+                    int left=0;
+                if(i>0) up=dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+                dp[i][j]=left+up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
