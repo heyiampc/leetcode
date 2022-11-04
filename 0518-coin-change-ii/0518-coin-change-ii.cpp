c9711d3d54fafb3b6fbbcfe3aa809a75278726eb
Prince Chaurasia <prncchaurasia@gmail.com>
2022-11-04 11:18:49 +0530

    Time: 116 ms (16.48%), Space: 18.4 MB (53.20%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Memoization
     int fun(int ind, vector<int> &coins, int T, vector<vector<int>> &dp)
     {
         if(ind==0) {
@@ -13,7 +14,22 @@ public:
         return dp[ind][T]=take+nottake;
     }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        return fun(coins.size()-1,coins,amount,dp);
+        // vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+        // return fun(coins.size()-1,coins,amount,dp);
+        //Tabulation
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,0));
+        int n=coins.size();
+        for(int T=0;T<=amount;T++)
+            dp[0][T]=(T%coins[0]==0);
+        for(int ind=1;ind<n;ind++)
+            for(int T=0;T<=amount;T++)
+            {
+                int nottake=dp[ind-1][T];
+                int take=0;
+                if(coins[ind]<=T)
+                    take=dp[ind][T-coins[ind]];
+                dp[ind][T]=take+nottake;
+            }
+        return dp[n-1][amount];
     }
 };
