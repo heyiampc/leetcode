e99208f2e1346a8061b8b1e0e52b1d48208c3ddd
Prince Chaurasia <prncchaurasia@gmail.com>
2022-11-07 23:32:01 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (82.75%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string str=to_string(num);
+        int i=0;
+        for(auto x:str)
+        {
+            int num1=x-'0';
+            if(num1!=9)
+            {str[i]='9';
+             break;
+            }
+            i++;
+        }
+        int ans=0;
+        for(int i=0;i<str.size();i++)
+        {
+            ans=(ans*10)+str[i]-'0';
+        }
+        return ans;
+    }
+};
