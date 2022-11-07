efe28c0df399859ff5d41a4734a85bf23ca00e9d
Prince Chaurasia <prncchaurasia@gmail.com>
2022-11-07 23:35:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -2,21 +2,28 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string str=to_string(num);
-        int i=0;
-        for(auto x:str)
-        {
-            int num1=x-'0';
-            if(num1!=9)
-            {str[i]='9';
-             break;
+        // int i=0;
+        // for(auto x:str)
+        // {
+        //     int num1=x-'0';
+        //     if(num1!=9)
+        //     {str[i]='9';
+        //      break;
+        //     }
+        //     i++;
+        // }
+        // int ans=0;
+        // for(int i=0;i<str.size();i++)
+        // {
+        //     ans=(ans*10)+str[i]-'0';
+        // }
+        // return ans;
+        for(auto &x:str)
+            if(x=='6')
+            {
+                x='9';
+                break;
             }
-            i++;
-        }
-        int ans=0;
-        for(int i=0;i<str.size();i++)
-        {
-            ans=(ans*10)+str[i]-'0';
-        }
-        return ans;
+        return stoi(str);
     }
 };
