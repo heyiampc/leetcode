ccf1e10e4373128272ba2ddb44034cca615e0b04
Prince Chaurasia <prncchaurasia@gmail.com>
2022-12-26 07:09:20 +0530

    Time: 15 ms (80.50%), Space: 14.4 MB (36.94%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -28,7 +28,30 @@ public:
         int n=prices.size();
         long profit=0;
         //n-> size 2-> 0/1 buy or not buy cap-> 0/1/2
-        vector<vector<vector<int> > > dp(n, vector<vector<int> >(2, vector<int>(k+1, -1)));
-        return fun(prices,0,1,k,dp,profit);
+        // vector<vector<vector<int> > > dp(n, vector<vector<int> >(2, vector<int>(k+1, -1)));
+        // return fun(prices,0,1,k,dp,profit);
+        //Tabulation
+         vector<vector<vector<int> > > dp(n+1, vector<vector<int> >(2, vector<int>(k+1,0)));
+        for(int index=n-1;index>=0;index--)
+            for(int buy=0;buy<=1;buy++)
+                for(int cap=1;cap<=k;cap++)
+                {
+                    if(buy)
+        {
+            //buy
+            long profit1=-prices[index]+dp[index+1][0][cap];
+            long profit2=0+dp[index+1][1][cap];
+            profit= max(profit1,profit2);
+        }
+        else
+        {
+            //sell
+            long profit1=prices[index]+dp[index+1][1][cap-1];
+            long profit2=0+dp[index+1][0][cap];
+            profit= max(profit1,profit2);
+        }
+        dp[index][buy][cap]=profit;
+                }
+        return dp[0][1][k];
     }
 };
