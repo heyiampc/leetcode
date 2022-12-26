d516a874474c0b426bffca487b8b18bc2b236942
Prince Chaurasia <prncchaurasia@gmail.com>
2022-12-26 07:00:52 +0530

    Time: 697 ms (44.12%), Space: 207.1 MB (29.66%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -28,7 +28,38 @@ public:
         int n=prices.size();
         long profit=0;
         //n-> size 2-> 0/1 buy or not buy cap-> 0/1/2
-         vector<vector<vector<int> > > dp(n, vector<vector<int> >(2, vector<int>(3, -1)));
-        return fun(prices,0,1,2,dp,profit);
+         // vector<vector<vector<int> > > dp(n, vector<vector<int> >(2, vector<int>(3, -1)));
+        // return fun(prices,0,1,2,dp,profit);
+        //Tabulation
+         vector<vector<vector<int> > > dp(n+1, vector<vector<int> >(2, vector<int>(3,0)));
+        //Base Case
+        // for(int i=0;i<n;i++)
+        //     for(int j=0;j<=2;j++)
+        //         dp[i][j][0]=0;
+        // for(int j=0;j<=1;j++)
+        //     for(int cap=0;cap<=2;cap++)
+        //         dp[n][j][cap]=0;
+        
+        for(int index=n-1;index>=0;index--)
+            for(int buy=0;buy<=1;buy++)
+                for(int cap=1;cap<=2;cap++)
+                {
+                    if(buy)
+        {
+            //buy
+            long profit1=-prices[index]+dp[index+1][0][cap];
+            long profit2=0+dp[index+1][1][cap];
+            profit= max(profit1,profit2);
+        }
+        else
+        {
+            //sell
+            long profit1=prices[index]+dp[index+1][1][cap-1];
+            long profit2=0+dp[index+1][0][cap];
+            profit= max(profit1,profit2);
+        }
+        dp[index][buy][cap]=profit;
+                }
+        return dp[0][1][2];
     }
 };
