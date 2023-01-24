211f5dadd7191183e0d92f919b403077bb6e18fb
Prince Chaurasia <prncchaurasia@gmail.com>
2023-01-25 00:18:46 +0530

    Time: 59 ms (90.10%), Space: 10.3 MB (74.31%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -19,7 +19,24 @@ public:
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(c+2,vector<int>(c+2,-1));
-        return fun(cuts,1,c,dp);
+        // vector<vector<int>> dp(c+2,vector<int>(c+2,-1));
+        // return fun(cuts,1,c,dp);
+        
+        //Tabulation
+        vector<vector<int>> dp(c+2,vector<int>(c+2,0));
+        for(int i=c;i>=1;i--)
+            for(int j=0;j<=c;j++)
+            {
+                if(i>j)
+                    continue;
+        int mini=INT_MAX;
+        for(int ind=i;ind<=j;ind++)
+        {
+            int cost=cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+            mini=min(mini,cost);
+        }
+                dp[i][j]=mini;
+        }
+        return dp[1][c];
     }
 };
