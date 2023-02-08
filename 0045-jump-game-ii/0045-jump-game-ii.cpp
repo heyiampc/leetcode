bd5cb23646efebb624ded44959fe5c76fdceca1b
Prince Chaurasia <prncchaurasia@gmail.com>
2023-02-08 22:52:39 +0530

    Time: 25 ms (56.16%), Space: 16.7 MB (56.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -13,28 +13,26 @@ public:
         }
         return dp[start]=minjump;
     }
-     int jump(vector<int>& nums)
-     {
-         vector<int> dp(nums.size(),-1);
-         return fun(nums,0,nums.size()-1,dp);
-         
-         
-         
-     }
-    // int jump(vector<int>& nums) {
-    //     //Using Greedy Approach
-    //     int jump=0;
-    //     int pos=0;
-    //     int max_reach=0;
-    //     for(int i=0;i<nums.size()-1;i++)
-    //     {
-    //         max_reach=max(max_reach,i+nums[i]);
-    //         if(pos==i)
-    //         {
-    //             pos=max_reach;
-    //             jump++;
-    //         }
-    //     }
-    //     return jump;
-    // }
+    //DP Soultion
+     // int jump(vector<int>& nums)
+     // {
+     //     vector<int> dp(nums.size(),-1);
+     //     return fun(nums,0,nums.size()-1,dp);
+     // }
+    int jump(vector<int>& nums) {
+        //Using Greedy Approach
+        int jump=0;
+        int pos=0;
+        int max_reach=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            max_reach=max(max_reach,i+nums[i]);
+            if(pos==i)
+            {
+                pos=max_reach;
+                jump++;
+            }
+        }
+        return jump;
+    }
 };
