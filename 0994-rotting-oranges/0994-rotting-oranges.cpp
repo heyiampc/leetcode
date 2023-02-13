de6b0e391e81a1bc4531338b0810d556a1466a2c
Prince Chaurasia <prncchaurasia@gmail.com>
2023-02-14 01:09:25 +0530

    Time: 3 ms (96.61%), Space: 13.1 MB (59.80%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        //{{row,col},tim}
+        queue<pair<pair<int,int>,int>> q;
+        int vis[n][m];
+        int countfresh=0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
+                }
+                else
+                {
+                    vis[i][j]=0;
+                }
+                if(grid[i][j]==1)
+                    countfresh++;
+            }
+        int tim=0;
+        int drow[]={-1,0,+1,0};
+        int dcol[]={0,+1,0,-1};
+        int count=0;
+        
+        //bfs traversal
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            tim=max(tim,t);
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                //check not viseted already
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
+                {
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=2;
+                    count++;
+                }
+            }
+        }
+        if(count!=countfresh)
+            return -1;
+        return tim;
+    }
+};
