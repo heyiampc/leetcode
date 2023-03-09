f5f0e4245bb42e69c681e949ac53a80665986173
Prince Chaurasia <prncchaurasia@gmail.com>
2023-03-09 14:18:11 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (59.22%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        bool ans=false;
-        for(auto mat:matrix)
-            ans|=binary_search(mat.begin(),mat.end(),target);
-        return ans;
+        // bool ans=false;
+        // for(auto mat:matrix)
+        //     ans|=binary_search(mat.begin(),mat.end(),target);
+        // return ans;
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int left=0;
+        int right=(r*c)-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            int row=mid/c; // to find i (row)
+            int col=mid%c; // to find j (col)
+            if(matrix[row][col]==target)
+                return true;
+            if(matrix[row][col]<target)
+                left=mid+1;
+            else
+                right=mid-1;
+        }
+        return false;
     }
 };
