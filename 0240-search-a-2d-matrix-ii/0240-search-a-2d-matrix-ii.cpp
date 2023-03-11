5596656fe66e667e494076e1aef89a2b12105df2
Prince Chaurasia <prncchaurasia@gmail.com>
2023-03-11 17:17:51 +0530

    Time: 123 ms (56.78%), Space: 14.9 MB (73.34%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        bool ans=false;
-        for(auto &mat:matrix)
+        // bool ans=false;
+        // for(auto &mat:matrix)
+        // {
+        //     ans|=binary_search(mat.begin(),mat.end(),target);
+        //     if(ans)
+        //         return true;
+        // }
+        // return ans;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int left=0;
+        int right=col-1;
+        while(right>=0 && left<row)
         {
-            ans|=binary_search(mat.begin(),mat.end(),target);
-            if(ans)
+            if(matrix[left][right]==target)
                 return true;
+            if(matrix[left][right]>target)
+                right--;
+            else
+                left++;
         }
-        return ans;
+        return false;
     }
 };
