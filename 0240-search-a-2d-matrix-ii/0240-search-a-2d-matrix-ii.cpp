1ad5b740e36803082170944541b34c8a4a52b8d1
Prince Chaurasia <prncchaurasia@gmail.com>
2023-03-11 16:54:03 +0530

    Time: 340 ms (13.25%), Space: 15 MB (40.59%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        bool ans=false;
+        for(auto &mat:matrix)
+        {
+            ans|=binary_search(mat.begin(),mat.end(),target);
+            if(ans)
+                return true;
+        }
+        return ans;
+    }
+};
