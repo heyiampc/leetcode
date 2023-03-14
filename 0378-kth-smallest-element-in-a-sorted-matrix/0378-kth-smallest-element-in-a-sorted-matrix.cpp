b82b1aef70e04ca15c434fde3cd6733c6d0322b8
Prince Chaurasia <prncchaurasia@gmail.com>
2023-03-14 14:16:18 +0530

    Time: 23 ms (97.47%), Space: 13.2 MB (84.02%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,53 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
-        for(auto &x:matrix)
-            for(auto &j:x)
+    int rankof(vector<vector<int>> &matrix, int mid)
+    {
+        int rank=0;
+        int n=matrix.size();
+        int i=n-1, j=0;
+        while(i>=0 && j<n)
+        {
+            if(matrix[i][j]>mid)
+                i--;
+            else
             {
-                pq.push(j);
-                if(pq.size()>k)
-                    pq.pop();
-
+                rank+=i+1;
+                j++;
             }
-        return pq.top();
+        }
+        return rank;
+        
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         priority_queue<int> pq;
+//         for(auto &x:matrix)
+//             for(auto &j:x)
+//             {
+//                 pq.push(j);
+//                 if(pq.size()>k)
+//                     pq.pop();
+
+//             }
+//         return pq.top();
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int left=matrix[0][0];
+        int right=matrix[row-1][col-1];
+        int ans;
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            int rank=rankof(matrix,mid);
+            if(rank<k)
+            {
+                ans=mid;
+                left=mid+1;
+            }
+            else
+                right=mid-1;
+        }
+        cout<<rankof(matrix,13)<<endl;
+        return left;
+        
     }
 };
