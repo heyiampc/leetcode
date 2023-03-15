5f10e715b9b306cdce98b6df3b79c75dc085f50c
Prince Chaurasia <prncchaurasia@gmail.com>
2023-03-15 23:34:28 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (92.48%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+        int diff=1,direction=0,start=1;
+        while(n>1)
+        {
+            if(n&1 || direction==0) // n is odd or if n is odd the direction from left to right
+            {
+                start+=diff;
+            }
+            n/=2;
+            diff*=2;
+            direction=!direction;
+        }
+        return start;
+    }
+};
