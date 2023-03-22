96a6160489cfaf212c27bdffd83f75abc0994314
Prince Chaurasia <prncchaurasia@gmail.com>
2023-03-22 14:51:34 +0530

    Time: 3 ms (76.51%), Space: 11.9 MB (73.93%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,33 +11,53 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    vector<int> temp;
-    queue<TreeNode*> q;
-    void fun(TreeNode* root)
+    // Using Inorder Traversal
+    // vector<int> ans;
+    // vector<int> temp;
+    // queue<TreeNode*> q;
+    // void fun(TreeNode* root)
+    // {
+    //     q.push(root);
+    //     while(!q.empty())
+    //     {
+    //         int n=q.size();
+    //         for(int i=0;i<n;i++)
+    //         {
+    //         TreeNode* front=q.front();
+    //         q.pop();
+    //         if(front->left)
+    //             q.push(front->left);
+    //         if(front->right)
+    //             q.push(front->right);   
+    //         temp.push_back(front->val);
+    //         }
+    //         ans.push_back(temp[temp.size()-1]);
+    //         temp.clear();
+    //     }
+    // }
+    // vector<int> rightSideView(TreeNode* root) {
+    //     if(root==NULL)
+    //     return ans;
+    //     fun(root);
+    //     return ans;
+    // }
+    
+    //Alternate Soluttion
+    void fun(TreeNode* root,int level,vector<int> &ans)
     {
-        q.push(root);
-        while(!q.empty())
-        {
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {
-            TreeNode* front=q.front();
-            q.pop();
-            if(front->left)
-                q.push(front->left);
-            if(front->right)
-                q.push(front->right);   
-            temp.push_back(front->val);
-            }
-            ans.push_back(temp[temp.size()-1]);
-            temp.clear();
-        }
+        if(root==NULL)
+            return;
+        if(ans.size()==level)
+            ans.push_back(root->val);
+        fun(root->right,level+1,ans);
+        fun(root->left,level+1,ans);
     }
-    vector<int> rightSideView(TreeNode* root) {
+        vector<int> rightSideView(TreeNode* root) {
+            vector<int> ans;
         if(root==NULL)
         return ans;
-        fun(root);
+        fun(root,0,ans);
         return ans;
     }
+    
 };
