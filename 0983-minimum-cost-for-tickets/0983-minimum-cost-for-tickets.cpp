73075f9ac04ade6073d37424468ad5eaf4a8cf00
Prince Chaurasia <prncchaurasia@gmail.com>
2023-03-28 22:10:29 +0530

    Time: 9 ms (22.34%), Space: 9.8 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -18,8 +18,28 @@ public:
         int onemonth=costs[2]+fun(days,costs,dp,i);
         return dp[ind]=min(oneday,min(sevenday,onemonth));
     }
+    int fun1(vector<int>& days, vector<int>& costs, vector<int> &dp, int ind)
+    {
+        //Base Case
+        if(ind>=days.size())
+            return 0;
+        if(dp[ind]!=-1)
+            return dp[ind];
+        //One Day Pass
+        int oneday=costs[0]+fun1(days,costs,dp,ind+1);
+        // Seven Day Pass
+        // int i;
+        // for(i=ind;i<days.size() && days[i]< days[ind]+7 ;i++);
+        int x=lower_bound(days.begin(),days.end(),days[ind]+7)-days.begin();
+        int sevenday=costs[1]+fun1(days,costs,dp,x);
+        //One Month
+        // for(i=ind;i<days.size()  && days[i]<days[ind]+30 ;i++);
+        int y=lower_bound(days.begin(),days.end(),days[ind]+30)-days.begin();
+        int onemonth=costs[2]+fun1(days,costs,dp,y);
+        return dp[ind]=min(oneday,min(sevenday,onemonth));
+    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         vector<int> dp(days.size()+1,-1);
-        return fun(days,costs,dp,0);
+        return fun1(days,costs,dp,0);
     }
 };
