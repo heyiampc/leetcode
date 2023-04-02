362c404483b8429ad061225d041850e213becead
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-02 17:25:31 +0530

    Time: 157 ms (32.33%), Space: 92.5 MB (9.05%) - LeetHub

---
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    //Max subarray Sum
+    int maxi;
+    int fun(vector<int> &nums, int n)
+    {
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int prev=fun(nums,n-1)+nums[n-1];
+        int curr=max(nums[n-1],prev);
+        maxi=max(maxi,curr);
+        return curr;
+    }
+    int maxSubArray(vector<int>& nums) {
+        maxi=nums[0];
+        fun(nums,nums.size());
+        return maxi;
+    }
+    
+    int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        vector<int> num1dif(n,0);
+        vector<int> num2dif(n,0);
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<n;i++)
+        {
+            num1dif[i]=nums1[i]-nums2[i];
+            num2dif[i]=nums2[i]-nums1[i];
+            sum1+=nums1[i];
+            sum2+=nums2[i];
+        }
+        int ans1=maxSubArray(num1dif)+sum2;
+        int ans2=maxSubArray(num2dif)+sum1;
+        return max(ans1,ans2);
+    }
+};
