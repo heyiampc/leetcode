3493fb33db25f92785a7e4999fa40cbff9a6092a
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-07 13:19:12 +0530

    Time: 78 ms (63.53%), Space: 29.3 MB (62.13%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -15,38 +15,38 @@ public:
     int numEnclaves(vector<vector<int>>& g) {
         int n=g.size();
         int m=g[0].size();
-        // //1st Row
-        // for(int i=0;i<m;i++)
-        //     if(g[0][i]==1)
-        //         dfs(0,i,g);
-        // //las Row
-        // for(int i=0;i<m;i++)
-        //     if(g[n-1][i]==1)
-        //         dfs(n-1,i,g);
-        // //1st col
-        // for(int i=0;i<m;i++)
-        //     if(g[i][0]==1)
-        //         dfs(i,0,g);
-        // //last col
-        // for(int i=0;i<m;i++)
-        //     if(g[m-1][i]==1)
-        //         dfs(m-1,i,g);
-        
-        int count=0;
+        //1st Row
+        for(int i=0;i<m;i++)
+            if(g[0][i]==1)
+                dfs(0,i,g);
+        //las Row
+        for(int i=0;i<m;i++)
+            if(g[n-1][i]==1)
+                dfs(n-1,i,g);
+        //1st col
         for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(i==0 || i==n-1 || j==0 || j==m-1)
-                {
-                    if(g[i][j]==1)
-                    {
-                        dfs(i,j,g);
-                    }
-                }
-            }
-        }
-                for(int i=0;i<n;i++)
+            if(g[i][0]==1)
+                dfs(i,0,g);
+        //last col
+        for(int i=0;i<n;i++)
+            if(g[i][m-1]==1)
+                dfs(i,m-1,g);
+        int count=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //         if(i==0 || i==n-1 || j==0 || j==m-1) // Boundary dfs only
+        //         {
+        //             if(g[i][j]==1)
+        //             {
+        //                 dfs(i,j,g);
+        //             }
+        //         }
+        //     }
+        // }
+        //Counting ones
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
