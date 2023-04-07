770af49045696521aa83cad1ff7e14d66039cb26
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-07 14:20:46 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -5,39 +5,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    // void dfs(vector<vector<int>>& image,int i,int j, int oc, int nc)
-    // {
-    //     if(i<0 || i==image.size() || j<0 || j==image[0].size())
-    //     return;
-    //     if(image[i][j]!=oc)
-    //     return;
-    //     image[i][j]=nc;
-    //     dfs(image,i,j-1,oc,nc);
-    //     dfs(image,i,j+1,oc,nc);
-    //     dfs(image,i-1,j,oc,nc);
-    //     dfs(image,i+1,j,oc,nc);
-    // }
-    // vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int nc) {
-    //     // Code here
-    //     int oc=image[sc][sr];
-    //     if(oc==nc)
-    //     return image;
-    //     dfs(image,sc,sr,oc,nc);
-    //     return image;
-    // }
-        void fill(vector<vector<int>>& image, int x, int y, int color, int oldColor,int n, int m)
+    void dfs(vector<vector<int>>& image,int i,int j, int oc, int nc)
     {
-        if(x<0 || y<0 || x==n || y==m || image[x][y]!=oldColor) return;
-        image[x][y]=color;
-        fill(image,x,y+1,color,oldColor,n,m);
-        fill(image,x+1,y,color,oldColor,n,m);
-        fill(image,x,y-1,color,oldColor,n,m);
-        fill(image,x-1,y,color,oldColor,n,m);
+        if(i<0 || i==image.size() || j<0 || j==image[0].size())
+        return;
+        if(image[i][j]!=oc)
+        return;
+        image[i][j]=nc;
+        dfs(image,i,j-1,oc,nc);
+        dfs(image,i,j+1,oc,nc);
+        dfs(image,i-1,j,oc,nc);
+        dfs(image,i+1,j,oc,nc);
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int oldColor=image[sr][sc];
-        if(color==oldColor) return image; //IMP else infinite recursive call
-        fill(image,sr,sc,color,oldColor,image.size(),image[0].size());
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int nc) {
+        // Code here
+        int oc=image[sr][sc];
+        if(oc==nc)
+        return image;
+        dfs(image,sr,sc,oc,nc);
         return image;
     }
 };
