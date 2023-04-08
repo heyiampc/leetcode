8ab228510d2822c19d7998bdc92428c4a57b184b
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-09 01:52:34 +0530

    Time: 20 ms (83.07%), Space: 13.7 MB (65.18%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool dfs(int i, vector<int> adj[], vector<int> &vis, vector<int> &pathvis)
+    {
+        vis[i]=1;
+        pathvis[i]=1;
+        
+        for(auto &x:adj[i])
+        {
+            if(!vis[x])
+            {
+            if(!dfs(x,adj,vis,pathvis))
+            {
+            return false;
+            }
+            }
+            else if(vis[x] && pathvis[x])
+            return false;
+        }
+        pathvis[i]=0;
+        return true;
+    }
+    //Detect cycle in an undirected graph
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for(int i = 0; i < prerequisites.size(); i++) {
+            int v = prerequisites[i][0];
+            int u = prerequisites[i][1];
+            adj[u].push_back(v);
+        }
+        int V=numCourses;
+        vector<int> vis(V,0);
+        vector<int> pathvis(V,0);
+        
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            if(!dfs(i,adj,vis,pathvis))
+            return false;
+        }
+        return true;
+    }
+};
