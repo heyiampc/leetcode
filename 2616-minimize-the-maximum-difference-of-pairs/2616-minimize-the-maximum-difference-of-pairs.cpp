93ad2c5b957741a7387005b9f7edced4ef94e834
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-09 17:30:14 +0530

    Time: 207 ms (25.00%), Space: 81.2 MB (25.00%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,38 @@
+#define ll long long
+class Solution {
+private:
+    bool isPossible(vector<int> &nums, ll mid,ll p)
+    {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
+        {
+            if(abs(nums[i]-nums[i+1])<=mid)
+            {
+                p--;
+                i++;
+            }
+        }
+        return p<=0;
+    }
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        if(p==0)
+            return 0;
+        ll left=0;
+        ll right=*max_element(nums.begin(),nums.end());
+        ll ans=INT_MAX;
+        sort(nums.begin(),nums.end());
+        while(left<=right)
+        {
+            ll mid=left+(right-left)/2;
+            if(isPossible(nums,mid,p))
+            {
+                ans=min(ans,mid);
+                right=mid-1;
+            }
+            else
+                left=mid+1;
+        }
+        return (int)ans;
+    }
+};
