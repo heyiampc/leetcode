687ec4f4370f181b5e425451155a53242ab01f58
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-19 00:38:48 +0530

    Time: 22 ms (77.74%), Space: 13 MB (92.32%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector< pair<int,int> >> graph(n);
-        for( auto edge: flights ){
-            graph[edge[0]].push_back( {edge[1],edge[2]} );
+        vector<pair<int,int>> adj[n];
+        for(auto &it:flights)
+        {
+            adj[it[0]].push_back({it[1],it[2]});
         }
         
-        vector<int> dist(n,pow(10,4)*n) , prev(n);
-        dist[src] = 0;
-        prev = dist;
-        k++;
-        while( k-- ){
-            for( int i = 0 ; i < n ; i++ ){
-                for( int j = 0; j < graph[i].size(); j++ ){
-                    int index = graph[i][j].first;
-                    int value = graph[i][j].second;
-                    // at x'th step there can be more than one updates
-                    // so we compare the update value, of the index 
-                    // gettting updated 
-                    if( dist[ index ] > prev[i] + value ){
-                        dist[ index ] = prev[i] + value;
-                    }
+        queue<pair<int,pair<int,int>>> q;
+        //stops,node,dist
+        q.push({0,{src,0}});
+        vector<int> dist(n,1e9);
+        dist[src]=0;
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+            int node=it.second.first;
+            int stops=it.first;
+            int cost=it.second.second;
+            if(stops>k)
+                continue;
+            for(auto &iter:adj[node])
+            {
+                int adjnodes=iter.first;
+                int adjw=iter.second;
+                
+                if(cost+adjw<dist[adjnodes] && stops<=k)
+                {
+                    dist[adjnodes]=cost+adjw;
+                    q.push({stops+1,{adjnodes,cost+adjw}});
                 }
             }
-            prev = dist;
         }
-        if( prev[dst] == pow(10,4)*n ){
-            return -1;
-        }
-        return prev[dst];
+        
+        return (dist[dst]!=1e9)?dist[dst]:-1;
     }
 };
