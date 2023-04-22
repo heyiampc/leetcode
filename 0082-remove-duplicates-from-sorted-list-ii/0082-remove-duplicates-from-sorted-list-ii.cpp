a30c85750a58f151862ffbd4dcbf50b3bf9e7fd7
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-22 09:29:48 +0530

    Time: 9 ms (56.46%), Space: 11.2 MB (24.11%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,24 +11,38 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        map<int,int> mp;
-        ListNode* temp=head;
-        while(temp)
-        {
-            mp[temp->val]++;
-            temp=temp->next;
+        //Map solution
+        // map<int,int> mp;
+        // ListNode* temp=head;
+        // while(temp)
+        // {
+        //     mp[temp->val]++;
+        //     temp=temp->next;
+        // }
+        // ListNode* dummy = new ListNode(-1);
+        // ListNode* p1=dummy;
+        // for(auto &x:mp)
+        // {
+        //     if(x.second==1)
+        //     {
+        //         ListNode* newnode= new ListNode(x.first);
+        //         dummy->next=newnode;
+        //         dummy=dummy->next;
+        //     }
+        // }
+        // return p1->next;
+        
+        //LL solution
+        ListNode *dummy=new ListNode(0,head);
+        ListNode *prev=dummy;
+        while(head!=NULL){
+            if(head->next!=NULL && head->val==head->next->val){
+                while(head->next!=NULL && head->val==head->next->val)head=head->next;
+                        prev->next=head->next;
+                }
+                else prev=prev->next;
+                head=head->next;
         }
-        ListNode* dummy = new ListNode(-1);
-        ListNode* p1=dummy;
-        for(auto &x:mp)
-        {
-            if(x.second==1)
-            {
-                ListNode* newnode= new ListNode(x.first);
-                dummy->next=newnode;
-                dummy=dummy->next;
-            }
-        }
-        return p1->next;
+        return dummy->next;
     }
 };
