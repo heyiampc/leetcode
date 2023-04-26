9e0f92ce25db1d9a5674631a89999875c893a5ab
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-26 10:00:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int addDigits(int num) {       
-        int sum=0;
-        do
-        {
-            sum=0;
-        while(num)
-        {
-            // cout<<sum<<" "<<num<<endl;
-            sum+=num%10;
-            num/=10;
-        }
-            num=sum;
-    }while(sum>9);
-        return sum;
+    int addDigits(int num) {      \
+        //O(N) Solution
+//         int sum=0;
+//         do
+//         {
+//             sum=0;
+//         while(num)
+//         {
+//             // cout<<sum<<" "<<num<<endl;
+//             sum+=num%10;
+//             num/=10;
+//         }
+//             num=sum;
+//     }while(sum>9);
+        
+//         return sum;
+        
+        //O(1) Solution
+        return 1+(num-1)%9;
     }
 };
