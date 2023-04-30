4f9d928b88ebe4bb047d430dd5160d39003722bd
Prince Chaurasia <prncchaurasia@gmail.com>
2023-04-30 23:29:54 +0530

    Time: 159 ms (18.38%), Space: 77.2 MB (92.07%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -9,18 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#define MAX(A,B) (A>B)?A:B
 class Solution {
 public:
-    long res = 0, total = 0, sub;
-    int maxProduct(TreeNode* root) {
-        total = s(root), s(root);
-        return res % (int)(1e9 + 7);
+    long long total;
+    long long maxi=1;
+    const int MOD=1e9+7;
+    using ll=long long;
+    void totsum(TreeNode* root)
+    {
+        if(root==NULL)
+            return;
+        if(root->left)
+            totsum(root->left);
+        if(root->right)
+            totsum(root->right);
+        total+=root->val;
     }
-
-    int s(TreeNode* root) {
-        if (!root) return 0;
-        sub = root->val + s(root->left) + s(root->right);
-        res = max(res, sub * (total - sub));
-        return sub;
+    ll subtreesum(TreeNode* root)
+    {
+        if(root==NULL)
+            return 0;
+        ll l=subtreesum(root->left);
+        ll r=subtreesum(root->right);
+        ll curr_sum=(l+r+root->val);
+        ll curr_pro=(curr_sum*(total-curr_sum));
+        maxi=MAX(maxi,curr_pro);
+        return curr_sum;
+    }
+    int maxProduct(TreeNode* root) {
+        totsum(root);
+        subtreesum(root);
+        return (maxi%MOD);
     }
 };
