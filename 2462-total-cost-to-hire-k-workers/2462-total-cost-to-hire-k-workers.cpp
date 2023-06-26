366d25357c1aff308665963bb7289e85dc6b78fa
Prince Chaurasia <prncchaurasia@gmail.com>
2023-06-26 21:29:22 +0530

    Time: 479 ms (9.41%), Space: 79.2 MB (12.47%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    using ll=long long;
+    long long totalCost(vector<int>& nums, int k, int candidates) {
+        priority_queue<ll,vector<ll>,greater<ll>> pq1;
+        priority_queue<ll,vector<ll>,greater<ll>> pq2;
+        vector<int> vis(nums.size(),0);
+        int i1=0;
+        while(i1<candidates){
+            vis[i1]=1;
+            pq1.push(nums[i1++]);
+        }
+        if(candidates > nums.size()- candidates){
+            candidates= nums.size()- candidates;
+        }
+        int i2=nums.size()-1;
+        int k1=candidates;
+        int k2=0;
+        while(k1--){
+            vis[i2]=1;
+            pq2.push(nums[i2--]);
+        }
+        // while(!pq1.empty()){
+        //     cout<<pq1.top()<<" ";
+        //     pq1.pop();
+        // }
+        // cout<<endl;
+        // while(!pq2.empty()){
+        //     cout<<pq2.top()<<" ";
+        //     pq2.pop();
+        // }
+        ll ans=0;
+        while(k2<k)
+        {
+            if(pq1.size()==0)
+            {
+                ans+=pq2.top();
+                pq2.pop();
+                if(i1<=i2)
+                    pq2.push(nums[i2--]);
+            }
+            else if(pq2.size()==0)
+            {
+                ans+=pq1.top();
+                pq1.pop();
+                if(i1<=i2)
+                    pq1.push(nums[i1++]);
+            }
+            else if(pq1.top()<=pq2.top())
+            {
+                ans+=pq1.top();
+                cout<<pq1.top()<<" ";
+                pq1.pop();
+                if(i1<nums.size() && i1<=i2){
+                    vis[i1]=1;
+                pq1.push(nums[i1++]);
+                }
+            }
+            else
+            {
+                ans+=pq2.top();
+                cout<<pq2.top()<<" ";
+                pq2.pop();
+                if(i2>=0 && i1<=i2){
+                    vis[i2]=1;
+                pq2.push(nums[i2--]);}
+                
+            }
+            k2++;
+        }
+        return ans;
+    }
+};
