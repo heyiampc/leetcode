ed49e1ade7e0342a102f993c1422bab21b36a38d
Prince Chaurasia <prncchaurasia@gmail.com>
2023-07-29 10:53:03 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (75.50%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -12,11 +12,24 @@ public:
         ll p1_j=nums[j]+min(fun(i+1,j-1,nums),fun(i,j-2,nums));
         return dp[i][j]=max(p1_i,p1_j);
     }
+    // Alternate solution using difference
+    ll fun1(int i,int j,vector<int> &nums)
+    {
+        if(i>j)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        ll p1_i=nums[i]-fun1(i+1,j,nums);
+        ll p1_j=nums[j]-fun1(i,j-1,nums);
+        return dp[i][j]=max(p1_i,p1_j);
+    }
     bool PredictTheWinner(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        ll player1=fun(0,nums.size()-1,nums); // pl score
-        ll total=accumulate(nums.begin(),nums.end(),0); // total score
-        ll player2=total-player1; // p2 score
-        return player1>=player2;
+        // ll player1=fun(0,nums.size()-1,nums); // pl score
+        // ll total=accumulate(nums.begin(),nums.end(),0); // total score
+        // ll player2=total-player1; // p2 score
+        // return player1>=player2;
+        
+        return fun1(0,nums.size()-1,nums)>=0;
     }
 };
