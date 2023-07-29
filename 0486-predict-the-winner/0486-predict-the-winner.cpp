9991448d7f39810511d27c5d895fb72e67fa7e1a
Prince Chaurasia <prncchaurasia@gmail.com>
2023-07-29 10:47:32 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (95.07%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     using ll=long long;
-    int n;
+    ll dp[21][21];
     ll fun(int i,int j,vector<int> &nums)
     {
         if(i>j)
             return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         ll p1_i=nums[i]+min(fun(i+2,j,nums),fun(i+1,j-1,nums));
         ll p1_j=nums[j]+min(fun(i+1,j-1,nums),fun(i,j-2,nums));
-        return max(p1_i,p1_j);
+        return dp[i][j]=max(p1_i,p1_j);
     }
     bool PredictTheWinner(vector<int>& nums) {
-        n=nums.size();
+        memset(dp,-1,sizeof(dp));
         ll player1=fun(0,nums.size()-1,nums); // pl score
         ll total=accumulate(nums.begin(),nums.end(),0); // total score
         ll player2=total-player1; // p2 score
