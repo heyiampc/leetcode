9f65485123e6db361b4a91b3960ee8fcf33746b6
Prince Chaurasia <prncchaurasia@gmail.com>
2023-08-08 13:21:42 +0530

    Time: 0 ms (100.00%), Space: 6 MB (77.17%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    map<int,int> mp;
+    int dp[20];
     int fun(int n)
     {
         if(n<=1)
             return 1;
-        if(n==2) return n;
         if(n==3) return 5;
         int ans=0;
-        if(mp.count(n))
-            return mp[n];
+        if(dp[n]!=-1)
+            return dp[n];
         for(int i=1;i<=n;i++)
             ans+=fun(i-1)*fun(n-i);
-        return mp[n]=ans;
+        return dp[n]=ans;
     }
     int numTrees(int n) {
+        memset(dp,-1,sizeof(dp));
         return fun(n);
     }
 };
