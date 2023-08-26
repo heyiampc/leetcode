9e8e8882255895e6c135b78512a13109a85ea03e
Prince Chaurasia <prncchaurasia@gmail.com>
2023-08-26 23:16:23 +0530

    Time: 752 ms (6.77%), Space: 50.2 MB (27.62%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    // map<pair<int,int>,int> dp;
-    long long dp[2500][2500];
+    int dp[2500][2500];
+    int x=1001;
     void print(vector<vector<int>> &nums)
     {
         for(auto &x:nums)
@@ -12,22 +12,20 @@ public:
     {
         if(i>=nums.size())
             return 0;
-        // if(dp.find({i,prev})!=dp.end())
-        //     return dp[{i,prev}];
-        if(dp[i+1001][prev+1001]!=-1)
-            return dp[i+1001][prev+1001];
+        if(dp[i+x][prev+x]!=-1)
+            return dp[i+x][prev+x];
         int take=0;
         if(nums[i][0]>prev){
             take=1+lis(i+1,nums,nums[i][1]);
         }
         int not_take=0+lis(i+1,nums,prev);
-        return dp[i+1001][prev+1001]=max(take,not_take);
+        return dp[i+x][prev+x]=max(take,not_take);
     }
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin(),pairs.end());
         // print(pairs);
         memset(dp,-1,sizeof(dp));
-        int ans=lis(0,pairs,-1001);
+        int ans=lis(0,pairs,-x);
         return ans;
     }
 };
