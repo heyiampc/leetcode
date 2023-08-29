81cac88e44fee13a87f1df11b0e8db4d0f97246a
Prince Chaurasia <prncchaurasia@gmail.com>
2023-08-29 11:34:55 +0530

    Time: 210 ms (6.56%), Space: 73.3 MB (5.66%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -16,23 +16,18 @@ public:
         int prefix=0;
         for(int i=0;i<n;i++)
         {
-            // presuf[i][0]=presuf[i-1][0];
-            // if(customers[i]=='N')
-            // presuf[i][0]++;
-            // if(presuf[i][0])
-            //     flag1=true;
+            if(i-1>=0)
+            presuf[i][0]=presuf[i-1][0];
             if(customers[i]=='N')
-                prefix++;
-            if(prefix+presuf[i+1][1]<mini)
+            presuf[i][0]++;
+            if(presuf[i][0]+presuf[i+1][1]<mini)
             {
-                mini=prefix+presuf[i+1][1];
+                mini=presuf[i][0]+presuf[i+1][1];
                 ind=i+1;
             }
         }
         // for(auto &it:presuf)
         //     cout<<it[0]<<" "<<it[1]<<endl;
-        // if(!flag1)
-        //     return n;
         return ind;
     }
 };
