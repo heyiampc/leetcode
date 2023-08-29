23f4b96088b85c111c11b878fbcb8feede0409a0
Prince Chaurasia <prncchaurasia@gmail.com>
2023-08-29 23:34:04 +0530

    Time: 600 ms (6.01%), Space: 24 MB (59.84%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,68 +1,36 @@
-class Solution {
-public:
-    bool fun(int i, int j, string &s1, string &s2, vector<vector<int>> &dp)
+class Solution
+{
+    public:
+    int dp[2001][2001];
+    bool fun(int i,int j, string &s1, string &s2)
     {
-        //Base Case
-        if(i==0 && j==0) return true;
-        if(i==0 && j>0) return false;
-        if(i>0 && j==0)
+        if(i>=s1.size() && j>=s2.size())
+            return true;
+        if(i>=s1.size() && j<s2.size())
         {
-            for(int ii=1;ii<=i;ii++)
-                if(s1[ii-1]!='*')
+            for(int k=j;k<s2.size();k++)
+                if(s2[k]!='*'){
+                    // cout<<"j"<<k<<endl;
                     return false;
+                }
             return true;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i-1]==s2[j-1] || s1[i-1]=='?')
-            return dp[i][j]=fun(i-1,j-1,s1,s2,dp);
-        if(s1[i-1]=='*')
+        if(i<s1.size() && j>=s2.size())
+            return false;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        bool ans=false;
+        if(s1[i]==s2[j] || s2[j]=='?')
+            ans|=fun(i+1,j+1,s1,s2);
+        if(s2[j]=='*')
         {
-            //fun(i-1,j,s1,s2) '*' as blank
-            //fun(i,j-1,s1,s2) '*' as s2[j]
-            return dp[i][j]=fun(i-1,j,s1,s2,dp) || fun(i,j-1,s1,s2,dp);
+            ans|=fun(i+1,j,s1,s2) | fun(i,j+1,s1,s2);
         }
-        return false;
+        return dp[i][j]=ans;
     }
-    // Recursion
-    // bool isMatch(string &s, string &p) {
-    //     int n=s.size();
-    //     int m=p.size();
-    //     vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-    //     return fun(m,n,p,s,dp);
-    // }
-    //Tabulation
-    bool isMatch(string &s2, string &s1) {
-        int n=s1.size();
-        int m=s2.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        dp[0][0]=true;
-        //Base Case
-        for(int j=1;j<=m;j++)
-            dp[0][j]=false;
-        for(int i=1;i<=n;i++)
-        {
-            for(int ii=1;ii<=i;ii++)
-                if(s1[ii-1]!='*') 
-                {dp[i][0]=false;
-                 break;
-                }
-            else
-                dp[i][0]=true;
-        }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-            if(s1[i-1]==s2[j-1] || s1[i-1]=='?')
-            dp[i][j]=dp[i-1][j-1];
-            else if(s1[i-1]=='*')
-           {
-            dp[i][j]=dp[i-1][j] || dp[i][j-1];
-           }
-            else
-                dp[i][j]=false;
-            }
-        }
-        return dp[n][m];
+    bool isMatch(string &s1, string &s2)
+    {
+        memset(dp,-1,sizeof(dp));
+        return fun(0,0,s1,s2);
     }
 };
