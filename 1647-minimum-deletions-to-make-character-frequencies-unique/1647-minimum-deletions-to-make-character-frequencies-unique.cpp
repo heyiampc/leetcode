27d096d8fc1f91daaee2b6dd6252088dbcb50a67
Prince Chaurasia <prncchaurasia@gmail.com>
2023-09-12 11:58:34 +0530

    Time: 127 ms (13.49%), Space: 17.6 MB (14.93%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -6,23 +6,26 @@ public:
             cout<<x<<" ";
         cout<<endl;
     }
-int makeElementsUnique(vector<int>& arr) {
-    unordered_map<int, int> frequency;
-    int operations = 0;
-    for (int num : arr) {
-        if (frequency.find(num) != frequency.end()) {
-            while (frequency.find(num) != frequency.end() && frequency[num] > 0) {
-                num--;
-                operations++;
+    int minop(vector<int> &nums)
+    {
+        map<int,int> mp;
+        int ans=0;
+        for(auto &it:nums)
+        {
+            if(mp.find(it)!=mp.end())
+            {
+                while(mp.find(it)!=mp.end() && mp[it]>0)
+                {
+                    it--;
+                    ans++;
+                }
+            }
+            if(it>0){
+                mp[it]++;
             }
         }
-        if (num > 0) {
-            frequency[num]++;
-        }
+        return ans;
     }
-
-    return operations;
-}
     int minDeletions(string s) {
         map<int,int> mp;
         for(auto &x:s)
@@ -34,7 +37,7 @@ int makeElementsUnique(vector<int>& arr) {
         }
         // sort(vec.begin(),vec.end());
         // print(vec);
-        int ans=makeElementsUnique(vec);
+        int ans=minop(vec);
         return ans;
     }
 };
