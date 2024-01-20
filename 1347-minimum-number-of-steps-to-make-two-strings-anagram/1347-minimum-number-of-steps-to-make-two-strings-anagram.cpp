915fa9620a5a906c077f6ca27b858c56256fa553
Prince Chaurasia <prncchaurasia@gmail.com>
2024-01-20 13:09:04 +0530

    Time: 47 ms (72.11%), Space: 17.7 MB (6.67%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        unordered_map<char,int> mp1,mp2;
+//         unordered_map<char,int> mp1,mp2;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             mp1[s[i]]++;
+//             mp2[t[i]]++;
+//         }
+//         int cnt=0;
+//         for(auto &[a,b]:mp2)
+//         {
+//             int c=mp1[a];
+//             if(mp1.find(a)!=mp1.end() && c>0 && b>0)
+//             {
+//                     int mini=min(mp1[a],mp2[a]);
+//                     mp1[a]-=mini;
+//                     mp2[a]-=mini;
+                
+//             }
+//             cnt+=b;
+//         }
+//         return cnt;
+        
+        //Alt Solution
+        int mp[26]={};
         for(int i=0;i<s.size();i++)
         {
-            mp1[s[i]]++;
-            mp2[t[i]]++;
+            mp[s[i]-'a']++;
+            mp[t[i]-'a']--;
         }
         int cnt=0;
-        for(auto &[a,b]:mp2)
+        for(int i=0;i<26;i++)
         {
-            int c=mp1[a];
-            if(mp1.find(a)!=mp1.end() && c>0 && b>0)
-            {
-                    int mini=min(mp1[a],mp2[a]);
-                    mp1[a]-=mini;
-                    mp2[a]-=mini;
-                
-            }
-            cnt+=b;
+            cnt+=abs(mp[i]);
         }
-        return cnt;
+        return cnt/2;
+        
+        
     }
 };
